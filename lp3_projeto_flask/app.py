43e5240a033d0a4e9c4fb0fcd3da69797ad9c0bf
Marino Yorinori <megumimariana@gmail.com>
2024-06-19 17:54:09 -0300

    implementação formulario

--- lp3_projeto_flask/app.py
+++ lp3_projeto_flask/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 lista_produtos = [
-    { "nome": "Coca-cola", "descricao": "veneno" },
-    { "nome": "Doritos", "descricao": "suja mão" },
-    { "nome": "Água", "descricao": "vida" },
-    { "nome": "Miasma", "descricao": "foda" }
+    { "nome": "Coca-cola", "descricao": "veneno", "preco": 4.40, "img": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.pngtree.com%2Ffree-water-png&psig=AOvVaw23-yLWu2SMm6a8j0SzDqe7&ust=1718915377438000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCJD0uvzA6IYDFQAAAAAdAAAAABAJ"},
+    { "nome": "Doritos", "descricao": "suja mão", "preco": 1.47, "img": "static/img/Doritos.png" },
+    { "nome": "Água", "descricao": "vida", "preco": 0.50, "img": "static/img/Água.png" },
+    { "nome": "Miasma", "descricao": "foda", "preco": 200.00, "img": "static/img/Miasma.png" }
 ]
 
 app = Flask(__name__)
@@ -31,3 +31,20 @@ def produto(nome):
         if produto['nome'] == nome:
             return render_template("produto.html", produto=produto)
     return 'Produto não existe!!!!!!'
+
+# GET, get é automatico do app.route
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro_produto.html")
+
+# method POST (recebe) - chamar o objeto request biblioteca
+@app.route("/produtos", methods=["POST"])
+def salvar_produto():
+    nome = request.form['nome'] #nome ta linkado com o name do formulario no html
+    descricao = request.form['descricao']
+    preco = float(request.form['preco'])
+    img = request.form['img']
+    produto = {"nome": nome, "descricao": descricao, "preco": preco, "img": img}
+    lista_produtos.append(produto) #append = função que adiciona no final da lista
+
+    return redirect(url_for("produtos")) # importar funcao redirect e url_for na briblioteca
