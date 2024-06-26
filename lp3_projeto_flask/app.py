8a816a9019cd9251b7ab63720e5bb7f4a1c4c876
Marino Yorinori <megumimariana@gmail.com>
2024-06-26 17:43:07 -0300

    read and append file

--- lp3_projeto_flask/app.py
+++ lp3_projeto_flask/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-lista_produtos = [
-    { "nome": "Coca-cola", "descricao": "veneno", "preco": 4.40, "img": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.pngtree.com%2Ffree-water-png&psig=AOvVaw23-yLWu2SMm6a8j0SzDqe7&ust=1718915377438000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCJD0uvzA6IYDFQAAAAAdAAAAABAJ"},
-    { "nome": "Doritos", "descricao": "suja mão", "preco": 1.47, "img": "static/img/Doritos.png" },
-    { "nome": "Água", "descricao": "vida", "preco": 0.50, "img": "static/img/Água.png" },
-    { "nome": "Miasma", "descricao": "foda", "preco": 200.00, "img": "static/img/Miasma.png" }
-]
+def obter_produtos():
+    lista_produtos = []
+    with open("produtos.csv", "r") as file:
+        linhas = file.readlines()
+        for linha in linhas:
+            print(linha.strip().split(","))
+            nome, descricao, valor, img = linha.strip().split(",")
+            produto = {
+                "nome": nome,
+                "descricao": descricao,
+                "valor": valor,
+                "img_url": img
+            }
+            lista_produtos.append(produto)
+        
+        return lista_produtos
+    
+def adicionar_produto(produto):
+    with open("produtos.csv", "a") as file:
+        linha = f"\n{produto['nome']},{produto['descricao']},{produto['valor']},{produto['img_url']}"
+        file.write(linha)
 
 app = Flask(__name__)
 # cria aplicação na memória que responde na porta 5000
@@ -21,20 +36,19 @@ def contato():
 
 @app.route("/produtos")
 def produtos():
-    return render_template("produtos.html", produtos=lista_produtos)
+    return render_template("produtos.html", produtos=obter_produtos())
 # PARA ACESSAR PAG HTML, é necessario usar a função importada render_template 
 # ele busca o arquivo em uma pasta de nome "templates" (precisa ser criada manualmente)
 
 @app.route("/produtos/<nome>")
 def produto(nome):
-    for produto in lista_produtos:
+    for produto in obter_produtos():
         if produto['nome'] == nome:
             return render_template("produto.html", produto=produto)
     return 'Produto não existe!!!!!!'
 
-# GET, get é automatico do app.route
-@app.route("/produtos/cadastro")
-def cadastro_produto():
+# GET, get é automatico do app.route salvar_produto():
+    nome = request.f
     return render_template("cadastro_produto.html")
 
 # method POST (recebe) - chamar o objeto request biblioteca
@@ -45,6 +59,9 @@ def salvar_produto():
     preco = float(request.form['preco'])
     img = request.form['img']
     produto = {"nome": nome, "descricao": descricao, "preco": preco, "img": img}
-    lista_produtos.append(produto) #append = função que adiciona no final da lista
+    adicionar_produto(produto)
 
     return redirect(url_for("produtos")) # importar funcao redirect e url_for na briblioteca
+
+if __name__ == "__main__":
+    app.run(port=5001)
